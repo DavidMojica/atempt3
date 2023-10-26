4efd21c1630b5cabbd683a89b4fe31845a13bc7f
David Mojica <davidmojicav@gmail.com>
2023-10-26 11:35:13 -0500

    save 1

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
